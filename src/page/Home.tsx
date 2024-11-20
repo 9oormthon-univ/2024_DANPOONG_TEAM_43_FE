928e9f4d8ab161ad829eb58ee82cfb54c54f3853
jiohjung98 <ckaquz98@naver.com>
2024-11-20 16:16:52 +0900

    feat: 메인 홈 컴포넌트 연결 및 배경 색 userType에 따라 변경

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from 'react';
+import UserGreeting from 'components/home/UserGreeting';
+import UserInfoCard from 'components/home/UserInfoCard';
+import NeighborSuggestions from 'components/home/NeighborSuggestions';
+import MapSection from 'components/home/MapSection';
+import Memories from 'components/home/Memories';
+import { useUserDataQuery } from 'service/user';
+import caregiverBg from '../assets/img/home/main-caregiver.svg';
+import volunteerBg from '../assets/img/home/main-volunteer.svg';
+import careWorkerBg from '../assets/img/home/main-careworker.svg';
 
-const Home = () => {
+const Home: React.FC = () => {
+  const { data: userData, isLoading } = useUserDataQuery();
+
+  if (isLoading) {
+    return null;
+  }
+
+  if (!userData) {
+    return null;
+  }
+
+  const backgroundSettings = {
+    CAREGIVER: { color: 'bg-[#ffe5e5]', image: caregiverBg },
+    VOLUNTEER: { color: 'bg-[#eef7ff]', image: volunteerBg },
+    CARE_WORKER: { color: 'bg-[#d8fbed]', image: careWorkerBg },
+  };
+
+  const { color, image } = backgroundSettings[userData.userType] || backgroundSettings['CAREGIVER'];
+
   return (
-    <div>Home</div>
-  )
-}
+    <div className={`relative ${color} max-w-[440px] min-w-[340px] w-full mx-auto overflow-y-auto min-h-screen pb-8`}>
+      <img
+        src={image}
+        alt="UserType Background"
+        className="absolute top-[3.8rem] right-[-140px] w-[100%] h-auto"
+        style={{ objectFit: 'cover', objectPosition: 'left' }}
+      />
+      <div className="w-[90%] mx-auto relative z-10">
+        <UserGreeting username={userData.username} userType={userData.userType} />
+        <UserInfoCard userType={userData.userType} city={userData.city} />
+        <NeighborSuggestions />
+        <MapSection userData={userData} />
+        <Memories />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
