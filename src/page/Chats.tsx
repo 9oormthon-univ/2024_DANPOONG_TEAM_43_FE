2f26ad1c72b290fdacfdaa83e974f0553b78b30b
sinyoung6491 <sinyoung6491@gmail.com>
2024-11-22 22:00:08 +0900

    🔥 [Del]Chat 필요없는 코드 삭제

--- src/page/Chats.tsx
+++ src/page/Chats.tsx
@@ -38,19 +38,6 @@ const Chats: React.FC = () => {
           />
         ))}
       </div>
-      <div className="group_chat">
-        <div className="title">모임 대화</div>
-        <div className="chat_room_div">
-          <img src={profile_group} alt="" className="profile" />
-          <div className="text">
-            <div className="top">
-              <p className="name">동행 간병</p>
-              <p className="when">오전 8:48</p>
-            </div>
-            <p className="contents">안녕하세요안녕하세요안녕하세요안녕하세요</p>
-          </div>
-        </div>
-      </div>
     </div>
   )
 }
