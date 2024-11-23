f8eecb620b7e1f7b9caacd914bb2542af2249929
sinyoung6491 <sinyoung6491@gmail.com>
2024-11-24 01:47:01 +0900

    💄 [Feat]채팅 빈화면 img 추가

--- src/page/Chats.tsx
+++ src/page/Chats.tsx
@@ -5,6 +5,7 @@ import PersonChat from 'components/chat/PersonChat';
 import axiosInstance from 'utils/axiosInstance';
 import { useQuery } from '@tanstack/react-query';
 import ConnectAI from 'components/home/ConnectAI';
+import chat_empty_back from '../assets/img/chat/chat_main_empty_back.svg'
 
 const fetchChatRooms = async () => {
   const response = await axiosInstance.get('/chat/rooms');
@@ -22,25 +23,33 @@ const Chats: React.FC = () => {
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Failed to fetch chat rooms.</div>;
 
+  const isChatRoomsEmpty = !chatRooms || chatRooms.length === 0;
 
   return (
     <div className='container' id='chat'>
       <div className="page_title">채팅</div>
-      <div className="person_chat">
-        <div className="title">이웃 대화</div>
-        {chatRooms && chatRooms.map((room: any) => (
-          <PersonChat
-            key={room.roomId}
-            roomId={room.roomId}
-            receiverName={room.receiverName}
-            lastMessage={room.lastMessage}
-            lastUpdated={room.lastUpdated}
-            receiverUserType={room.receiverUserType}
-            receiverId={room.receiverId}
-          />
-        ))}
-      </div>
-      <ConnectAI/>
+      {isChatRoomsEmpty ? (
+        // chatRooms가 비어 있을 때 빈 화면 이미지 보여주기
+        <div className="empty_chat">
+          <img src={chat_empty_back} alt="No chats available" />
+        </div>
+      ) : (
+        // chatRooms에 데이터가 있을 때 기존 UI
+        <div className="person_chat">
+          <div className="title">이웃 대화</div>
+          {chatRooms.map((room: any) => (
+            <PersonChat
+              key={room.roomId}
+              roomId={room.roomId}
+              receiverName={room.receiverName}
+              lastMessage={room.lastMessage}
+              lastUpdated={room.lastUpdated}
+              receiverUserType={room.receiverUserType}
+              receiverId={room.receiverId}
+            />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
