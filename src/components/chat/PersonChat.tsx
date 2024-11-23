89e5d1ae8f8b04d27b0c2338d01dc42b268a5769
sinyoung6491 <sinyoung6491@gmail.com>
2024-11-23 16:20:50 +0900

    ✨ [Feat]채팅방 생성 api

--- src/components/chat/PersonChat.tsx
+++ src/components/chat/PersonChat.tsx
@@ -6,6 +6,37 @@ import { useNavigate } from "react-router-dom";
 import { useMutation } from '@tanstack/react-query';
 import axiosInstance from 'utils/axiosInstance';
 
+import type1_1 from '../../assets/img/user/type1-1.svg';
+import type1_2 from '../../assets/img/user/type1-2.svg';
+import type1_3 from '../../assets/img/user/type1-3.svg';
+import type1_4 from '../../assets/img/user/type1-4.svg';
+import type1_5 from '../../assets/img/user/type1-5.svg';
+import type1_6 from '../../assets/img/user/type1-6.svg';
+import type1_7 from '../../assets/img/user/type1-7.svg';
+import type1_8 from '../../assets/img/user/type1-8.svg';
+import type1_9 from '../../assets/img/user/type1-9.svg';
+import type1_10 from '../../assets/img/user/type1-10.svg';
+import type2_1 from '../../assets/img/user/type2-1.svg';
+import type2_2 from '../../assets/img/user/type2-2.svg';
+import type2_3 from '../../assets/img/user/type2-3.svg';
+import type2_4 from '../../assets/img/user/type2-4.svg';
+import type2_5 from '../../assets/img/user/type2-5.svg';
+import type2_6 from '../../assets/img/user/type2-6.svg';
+import type2_7 from '../../assets/img/user/type2-7.svg';
+import type2_8 from '../../assets/img/user/type2-8.svg';
+import type2_9 from '../../assets/img/user/type2-9.svg';
+import type2_10 from '../../assets/img/user/type2-10.svg';
+import type3_1 from '../../assets/img/user/type3-1.svg';
+import type3_2 from '../../assets/img/user/type3-2.svg';
+import type3_3 from '../../assets/img/user/type3-3.svg';
+import type3_4 from '../../assets/img/user/type3-4.svg';
+import type3_5 from '../../assets/img/user/type3-5.svg';
+import type3_6 from '../../assets/img/user/type3-6.svg';
+import type3_7 from '../../assets/img/user/type3-7.svg';
+import type3_8 from '../../assets/img/user/type3-8.svg';
+import type3_9 from '../../assets/img/user/type3-9.svg';
+import type3_10 from '../../assets/img/user/type3-10.svg';
+
 
 interface PersonChatProps {
   roomId: string;
@@ -15,9 +46,24 @@ interface PersonChatProps {
   receiverUserType: string;
   receiverId:number;
 }
+const imageMapping: { [key: string]: string[] } = {
+  CAREGIVER: [type1_1, type1_2, type1_3, type1_4, type1_5, type1_6, type1_7, type1_8, type1_9, type1_10],
+  CARE_WORKER: [type2_1, type2_2, type2_3, type2_4, type2_5, type2_6, type2_7, type2_8, type2_9, type2_10],
+  VOLUNTEER: [type3_1, type3_2, type3_3, type3_4, type3_5, type3_6, type3_7, type3_8, type3_9, type3_10],
+};
+
+const getProfileImage = (userId: number, userType: string): string => {
+  const images = imageMapping[userType];
+  if (!images) return '';
+  const index = userId % images.length;
+  return images[index];
+};
 
 const PersonChat: React.FC<PersonChatProps> = ({ roomId, receiverName, lastMessage, lastUpdated, receiverUserType,receiverId }) => {
   const navigate = useNavigate();
+
+  // 매핑된 프로필 이미지를 가져옵니다.
+  const profileImage = getProfileImage(receiverId, receiverUserType);
 
   const openChatRoomMutation = useMutation({
     mutationFn: async () => {
@@ -31,7 +77,8 @@ const PersonChat: React.FC<PersonChatProps> = ({ roomId, receiverName, lastMessa
           roomId,
           receiverUserType,
           receiverName,
-          receiverId
+          receiverId,
+          profileImage,
         },
       });
     },
@@ -45,22 +92,9 @@ const PersonChat: React.FC<PersonChatProps> = ({ roomId, receiverName, lastMessa
     }
   }
 
-  // 사용자 타입에 따라 프로필 이미지 선택
-  const getProfileImage = () => {
-    switch (receiverUserType) {
-      case 'CARE_WORKER':
-        return profile_careworker;
-      case 'CAREGIVER':
-        return profile_caregiver;
-      case 'VOLUNTEER':
-        return profile_volunteer;
-      default:
-        return profile_caregiver; // 기본 이미지
-    }
-  };
   return (
     <div className="chat_room_div" onClick={GoToRoom}>
-      <img src={getProfileImage()} alt="" className="profile" />
+      <img src={profileImage} alt="" className="profile" />
       <div className="text">
         <div className="top">
           <p className="name">{receiverName}</p>
