42457d390731facc762db39504792fc2945b6ad8
sinyoung6491 <sinyoung6491@gmail.com>
2024-12-07 23:22:18 +0900

    🩹 [Fix]댓글 작성 후 댓글 수 업데이트

--- src/page/GroupFeedList.tsx
+++ src/page/GroupFeedList.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import new_post from '../assets/img/group/new_post_icon.svg';
 import back from '../assets/img/chat/chat-back.svg';
 import FeedPreview from 'components/group/FeedPreview';
-import { useFeedDataQuery } from 'service/useGroupQueries'; 
+import { useFeedDataQuery } from 'service/useGroupQueries';
 
 const GroupFeedList = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const { groupId } = location.state || {};
 
-  const { data: feeds, isLoading, isError } = useFeedDataQuery(groupId); 
+  const { data: feeds, isLoading, isError } = useFeedDataQuery(groupId);
+  const [refreshKey, setRefreshKey] = useState(0); // 리렌더링 트리거
 
+  useEffect(() => {
+    if (location.state?.refresh) {
+      setRefreshKey((prev) => prev + 1); // 강제 리렌더링
+    }
+  }, [location.state]);
   const handleBackClick = () => {
     navigate(-1);
   };
@@ -35,6 +41,7 @@ const GroupFeedList = () => {
           feeds.map((feed) => (
             <React.Fragment key={feed.newsId}>
               <FeedPreview
+                key={refreshKey}
                 title={feed.title}
                 content={feed.content}
                 writerType={feed.writerType}
